04d73748f3c8c8dd2189cf9d256d1795d32c4ad9
kyosima <btvu282@gmail.com>
2020-08-28 18:13:19 +0700

    28-08 Mac

--- database/support_mevivu_Users.sql
+++ database/support_mevivu_Users.sql
@@ -31,13 +31,13 @@ CREATE TABLE `Users` (
   `createdDate` datetime DEFAULT CURRENT_TIMESTAMP,
   `firstName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `lastName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT '3',
   `levels` tinyint DEFAULT NULL,
   `avatar` varchar(500) COLLATE utf8_unicode_ci DEFAULT 'Itachi.png',
   `birthDay` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `userName_UNIQUE` (`userName`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,7 +46,7 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (1,'mevivu','admin@gmail.com','123',0,'2020-08-17 15:00:06','bui','vu','12341',NULL,'123',NULL),(4,'admin','btvu2821@gmail.com','pbkdf2:sha256:150000$g3Z2SpDo$08dd7459b2601ed02e8ac70cd334d1ba000408e723198ba37a96c537c9cc6cfc',0,'2020-08-17 15:33:46','Bùi Thế','Vũ','335828494',1,'Itachi.png','2020-02-09'),(5,'admin1','ca@gmail.com','pbkdf2:sha256:150000$bA8SLz6K$9f6fc4b752ed6ac9db2d957394f805338c0fe063c72410a8d2b7cf56b6c7b44d',0,'2020-08-17 16:39:28','BÙI','VŨ',NULL,2,'Itachi.png',NULL),(6,'kyo','kyosima@gmail.com','pbkdf2:sha256:150000$bA8SLz6K$9f6fc4b752ed6ac9db2d957394f805338c0fe063c72410a8d2b7cf56b6c7b44d',0,'2020-08-26 12:03:16','A','A','1111',3,'Itachi.png',NULL),(7,'khachhang','an@gmail.com','pbkdf2:sha256:150000$bA8SLz6K$9f6fc4b752ed6ac9db2d957394f805338c0fe063c72410a8d2b7cf56b6c7b44d',0,'2020-08-26 17:18:26','Kyo','khách','1231313142',2,'Itachi.png','2020-08-14');
+INSERT INTO `Users` VALUES (1,'mevivu','chinhhung@gmail.com','123',0,'2020-08-17 15:00:06','Nguyễn Chính','Hưng ATM','01223412332',2,'123','2020-08-28'),(4,'admin','btvu2821@gmail.com','pbkdf2:sha256:150000$g3Z2SpDo$08dd7459b2601ed02e8ac70cd334d1ba000408e723198ba37a96c537c9cc6cfc',0,'2020-08-17 15:33:46','Bùi Thế','Vũ','335828494',1,'Itachi.png','2020-02-20'),(5,'admin1','quocminh@gmail.com','pbkdf2:sha256:150000$bA8SLz6K$9f6fc4b752ed6ac9db2d957394f805338c0fe063c72410a8d2b7cf56b6c7b44d',0,'2020-08-17 16:39:28','Nguyễn Quốc','Minh','0972321434',3,'Itachi.png','2020-04-26'),(6,'kyo','kyosima@gmail.com','pbkdf2:sha256:150000$bA8SLz6K$9f6fc4b752ed6ac9db2d957394f805338c0fe063c72410a8d2b7cf56b6c7b44d',0,'2020-08-26 12:03:16','Selena','Pham','01234123123',3,'Itachi.png','2020-08-25'),(7,'khachhang','an@gmail.com','pbkdf2:sha256:150000$bA8SLz6K$9f6fc4b752ed6ac9db2d957394f805338c0fe063c72410a8d2b7cf56b6c7b44d',0,'2020-08-26 17:18:26','Kyo','khách','1231313142',3,'Itachi.png','2020-08-14'),(8,'dangvy','masad@gmail.com','pbkdf2:sha256:150000$tTLX4UnV$dadf2c136d3086e61010a03b835c9d346261af490e173bc949791b5bbfbe3870',0,'2020-08-28 11:34:36','BÙI','VŨ',NULL,3,'Itachi.png',NULL),(9,'viethoang','aaaa@gmail.com','pbkdf2:sha256:150000$s4qOa8iZ$3f2d6dad97edb93af84ece6c666e02379c47df38d86dcdf8ec23ac61d61e59c1',0,'2020-08-28 11:41:23','Việt ','Hoàng','0123455678',2,'Itachi.png','2020-08-04'),(10,'quocthinh','abc@gmail.com','pbkdf2:sha256:150000$owdGgZ45$079ee2af65876ebd020539073e6f22cdd3ac12dbcd88043833118d6c219e9bb9',0,'2020-08-28 11:45:54','Quốc ','Thịnh','0943452242',2,'Itachi.png','2020-08-25'),(11,'quocthinh1','abc@gmail.com','pbkdf2:sha256:150000$UjO5chTR$474279e25b61cc297de7b280b76efe314b30dac5a3b30901384dcc01bb5e2535',0,'2020-08-28 11:47:46','Quốc','Thịnh','0335828494',3,'Itachi.png','2020-08-12'),(12,'tronghieu','hieu@gmail.com','pbkdf2:sha256:150000$b8EzGWAu$e7db6ac928b2896305e6107ac0a4559b5a82c880720bf58313cc7a0f5fb8c383',0,'2020-08-28 12:20:28','Trọng','Hiếu','0999999999',2,'Itachi.png','2020-08-07'),(13,'david','abc@gmail.com','pbkdf2:sha256:150000$ZXtTnlBG$a77d1c2adbaa9dad4d4d4bf3ff535b5b8d4f65deb91220b3e2dd891721b75748',0,'2020-08-28 14:32:02','Alaba','Davis','09892372931',2,'Itachi.png','2020-08-15'),(14,'vanluyen','aaaa@gmail.com','pbkdf2:sha256:150000$YbSBrFI0$2a05f3aecea19b1c8d2e3d96de74ef4d3c627e29ae244601094670211780384e',0,'2020-08-28 14:39:52','Lê Văn','Luyện','0987998832',3,'Itachi.png','2020-08-19'),(15,'kythuat','kythuat@gmail.com','pbkdf2:sha256:150000$hfauyrUP$6cfd3fecb40387c490502635032d4bc3ad37addddc89b9ef75e30869cbaa557d',0,'2020-08-28 16:45:34','Nhân viên','Kỹ thuật','7869846467',2,'kyo.jpg','2020-08-03');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -59,4 +59,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-26 17:55:55
+-- Dump completed on 2020-08-28 18:12:35
